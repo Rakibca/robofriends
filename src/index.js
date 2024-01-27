b5b58262a6c6609702dbfdc2e5ce1cb9d7783051
Rakibul Islam <rakib.ca@gmail.com>
2024-01-27 01:15:16 -0700

    Added Card & CardList components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import CardList from "./CardList";
+import { robots } from "./robots";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <h1>Hello Ashek !!</h1>
+    <CardList robots={robots} />
   </React.StrictMode>
 );
