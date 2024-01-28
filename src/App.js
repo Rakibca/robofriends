830727ccf8b845e9c9a3a1a155e62d4cb8d2a391
Rakibul Islam <rakib.ca@gmail.com>
2024-01-27 23:37:43 -0700

    CSS updates

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import SearchBox from "./SearchBox";
 
 const App = () => {
   return (
-    <>
+    <div className='tc'>
       <h1>RoboFriends</h1>
       <SearchBox />
       <CardList robots={robots} />
-    </>
+    </div>
   );
 };
 
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,6 +1,12 @@
 const SearchBox = () => {
-  return ( 
-  <input type="search" placeholder="search robots" />
+  return (
+    <div className="pa2">
+      <input
+        className="pa3 ba b--green bg-lightest-blue"
+        type="search"
+        placeholder="search robots"
+      />
+    </div>
   );
 };
 
