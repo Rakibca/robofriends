096d874792db255327e7c9004858d127f0874ffa
Rakibul Islam <rakib.ca@gmail.com>
2024-02-02 10:10:23 -0700

    Added components & containers folders

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import { Component } from "react";
-import CardList from "./CardList";
+import CardList from "../components/CardList";
 //import { robots } from "./robots";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
@@ -27,27 +27,24 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
 
     //console.log("render");
 
-    if (this.state.robots.length === 0) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="tc">
-          <h1 className="f1">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
-        </div>
-      );
-    }
+    return !robots.length ? (
+      <h1>Loading...</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./containers/App";
+import registerServiceWorker from "./registerServiceWorker";
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,3 +11,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+registerServiceWorker();
